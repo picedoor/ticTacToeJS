3cd8092512950645406701636b6ab72a6580d871
picedoor <sean247@naver.com>
2019-02-21 23:27:10 +0900

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,10 @@ function game_start() {
 	//alert("게임이 시작되었습니다!");
 	isStarted = true;
 	setTurn(now = 1);
-	for(var i = 0; i < 9; i++) board[i] = 0;
+	for(var i = 0; i < 9; i++) {
+		setOwner(i, 0);
+		board[i] = 0;
+	}
 }
 
 function check_finish() {
@@ -21,8 +24,18 @@ function check_finish() {
 	}
 	if(((board[0] == board[4])&&(board[4] == board[8])&&board[4] != 0)||((board[2] == board[4])&&(board[4] == board[6])&&board[4] != 0)) won = board[4];
 	if(won != 0) {
-		alert(won + "님이 이겼읍니다 *^^*");
+		alert(won + "님이 이겼읍니다 *^^*\n확인을 누르면 처음부터 다시 시작합니다.");
 		isStarted = false;
+		game_start();
+	}
+	var j = 0;
+	for(var i = 0; i < 9; i++) {
+		if(board[i] != 0) j++;
+		else break;
+	}
+	if(j == 9) {
+		alert("무승부입니다*^^*\n확인을 누르면 다시 시작합니다.");
+		game_start();
 	}
 }
 
